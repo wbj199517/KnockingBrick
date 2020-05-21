e0665ca2dbcd57e6e1812e335966b02459a39c84
brianwang <brianwang2013@hotmail.com>
2020-05-21 15:44:05 -0400

    add user list

--- client/main-app/src/component/chat-room/Chat/Chat.js
+++ client/main-app/src/component/chat-room/Chat/Chat.js
@@ -1,8 +1,70 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
+let socket;
 
-const Chat = () => {
+const Chat = ({location}) => {
+
+    const [name,setName] = useState('');
+    const [room,setRoom] = useState('');
+    const [messages,setMessages] = useState([]);
+    const [users, setUsers] = useState('');
+    const [message,setMessage] = useState('');
+    const ENDPOINT = 'https://knocking-brick.herokuapp.com/'
+
+    useEffect(()=>{
+        const {name,room} = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+        setName(name);
+        setRoom(room);
+        
+        socket.emit('join', {name,room},()=>{
+                // console.log(message);
+        });
+
+        return () =>{
+            socket.emit('disconnect');
+            socket.off();
+        }
+
+    },[ENDPOINT,location.search]);
+
+    useEffect(() => {
+        socket.on('message', message => {
+          setMessages(messages => [ ...messages, message ]);
+        });
+        
+        socket.on("roomData", ({ users }) => {
+          setUsers(users);
+        });
+    }, []);
+
+    const sendMessage = (event)=>{
+        event.preventDefault();
+
+        if(message){
+            socket.emit('sendMessage',message,()=> setMessage(''));
+        }
+    }
+    console.log(message,messages)
     return(
-        <h1>chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room}/>
+                <Messages messages={messages} name={name}/>
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+
+            </div>
+            <TextContainer users={users}/>
+             
+
+        </div>
     )
 }
 
